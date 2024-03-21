ba34c20a003491c32365a8909523ab63f7cad975
Darya <DarinaHreb@gmail.com>
2024-03-21 15:58:21 +0400

    add project SvelteKit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import myphoto from '$lib/images/myphoto.jpg';
+</script>
+
+
+<div class="container">
+<img alt="Мое фото" src={myphoto} width="400" />
+
+<h1>Обо мне</h1>
+<p>У меня много увлечений, и я хочу рассказатьо некоторых из них:
+    <br>1. Танцы. Я занимаюсь танцами и мне очеь нравится.
+    <br>2. Сериалы. Я люблю смотреть разные сериалы, особенно романтику.
+    <br>3. Гулять. Я очень люблю погулять со своими друзьями.
+    <br>Об остальном я расскажу в другой раз.
+</p>
+<h1> Как меня найти</h1>
+<p>Vk: @id664977960</p>
+<p>Tg: @Reeeeeeeeew</p>
+</div>
+
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+</style>
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import photo1 from "$lib/images/photo1.jpg";
+  import photo2 from "$lib/images/photo2.jpg";
+  import photo3 from "$lib/images/photo3.jpg";
+  import photo4 from "$lib/images/photo4.jpg";
+  import photo5 from "$lib/images/photo5.jpg";
+  import photo6 from "$lib/images/photo6.jpg";
+</script>
+
+<div class="container">
+  <img alt="фото котика 1" src={photo1} width="400" />
+  <br />
+  <img alt="фото котика 2" src={photo2} width="400" />
+  <br />
+  <img alt="фото котика 3" src={photo3} width="400" />
+  <br />
+  <img alt="фото котика 4" src={photo4} width="400" />
+  <br />
+  <img alt="фото котика 5" src={photo5} width="400" />
+  <br />
+  <img alt="фото котика 6" src={photo6} width="400" />
+</div>
+
+<style>
+  .container {
+    margin: 50px auto;
+    max-width: 800px;
+    padding: 0 20px;
+  }
+</style>
